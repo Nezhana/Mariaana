7cb1ac4258dd9460895b23a25834fa6529fc4810
Nezhana <104068404+Nezhana@users.noreply.github.com>
2024-02-29 13:04:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,43 @@
+
 # main file
+
+
+def input_recognize(user_input):
+    match user_input.lower():
+        case 'привіт':
+            message = 'І тобі привіт! Чим можу допомогти?'
+            status = 'answer'
+            return message, status
+        case 'вітаю':
+            message = 'Вітаю вас! Чим можу допомогти?'
+            status = 'answer'
+            return message, status
+        case 'добрий день':
+            message = 'Доброго дня. Чим можу допомогти?'
+            status = 'answer'
+            return message, status
+        case 'змінити формат усний':
+            message = 'Змінюю формат на: усний.'
+            status = 'voice'
+            return message, status
+        case 'змінити формат письмовий':
+            message = 'Змінюю формат на: письмовий.'
+            status = 'text'
+            return message, status
+        case 'змінити гучність':
+            message = 'Гучність змінена. Що я можу зробити для вас ще?'
+            status = 'answer'
+            return message, status
+        case 'змінити швидкість':
+            message = 'Швідкість змінена. Що я можу зробити для вас ще?'
+            status = 'answer'
+            return message, status
+        case _:
+            message = user_input
+            status = 'answer'
+            return message, status
+
+
 import streamlit as st
 st.title('Мар\'янка')
 
@@ -8,15 +47,17 @@ rate = st.slider('Для того, щоб змінити гучність, на�
 # st.write('Змінюю гучність на: ', volume)
 # st.write('Змінюю швидкість на: ', rate)
 
-txt = st.text_area(
+
+user_input = st.text_area(
     "Привiт! Скажiть щось або 'До побачення', щоб завершити.",
-    height=300, disabled=False,
+    height=140, disabled=False,
     help='Ось перелік основних команд:\n- змінити гучніть\n- змінити швидкість\n- змінити формат письмовий\n- змінити формат усний',)
 
 # if txt:
 #     st.write("You entered: ", txt)
 
 messages = st.container()
-if txt:
-    messages.chat_message("user", avatar='👨‍🎓').write(txt)
-    messages.chat_message("assistant", avatar='👩‍🦱').write(f"Echo: {txt}")
+if user_input:
+    message = input_recognize(user_input)
+    messages.chat_message("user", avatar='👨‍🎓').write(user_input)
+    messages.chat_message("assistant", avatar='👩‍🦰').write(f"Echo: {message}")
