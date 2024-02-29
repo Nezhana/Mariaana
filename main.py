4bc415f307f9a9372bb8268dcd896f3ec6172a74
Nezhana <104068404+Nezhana@users.noreply.github.com>
2024-02-29 12:34:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 # main file
 import streamlit as st
-st.title('Uber pickups in NYC')
+st.title('Мар'янка')
+
+volume = st.slider('Для того, щоб змінити швидкість, надайте значення від нуля до ста.', 0, 100, 100)
+rate = st.slider('Для того, щоб змінити гучність, надайте значення від нуля до ста.', 0, 100, 100)
+
+st.write(volume, rate)
